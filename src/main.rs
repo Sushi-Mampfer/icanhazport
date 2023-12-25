2408fc557269b793a88333e557e75eec619aff86
Sushi-Mampfer <tim.schwyzer@gmx.ch>
2023-12-25 21:51:13 +0100

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,9 @@ use tokio::time::{
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/:port", get(handle_request));
+    let app = Router::new()
+        .route("/:port", get(handle_request));
+//         .route("/", get(get_ip)):
 
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -23,6 +25,10 @@ async fn handle_request(Path(port): Path<u16>, headers: HeaderMap) -> Json<bool>
     let ip: &str = headers.get("CF-Connecting-IP").unwrap().to_str().unwrap();
     return Json(scan_port(SocketAddr::new(ip.parse().unwrap(), port)).await);
 }
+// async fn get_ip(headers: HeaderMap) -> u16 {
+//     let ip: u16 = headers.get("CF-Connecting-IP").unwrap().to_str().unwrap().parse();
+//     return ip;
+// }
 
 async fn scan_port(addr: SocketAddr) -> bool {
     if let Ok(_) =timeout(Duration::from_secs(3), TcpStream::connect(addr)).await {
