a517cb077aff666aebca63867a65cea634fa8dd2
Sushi-Mampfer <tim.schwyzer@gmx.ch>
2023-12-26 18:15:59 +0100

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,8 @@ async fn handle_request(Path(port): Path<u16>, headers: HeaderMap) -> Json<bool>
     let ip: &str = headers.get("CF-Connecting-IP").unwrap().to_str().unwrap();
     return Json(scan_port(SocketAddr::new(ip.parse().unwrap(), port)).await);
 }
-async fn get_ip(headers: HeaderMap) -> Json<u16> {
-    let ip: u16 = headers.get("CF-Connecting-IP").unwrap().to_str().unwrap().parse().unwrap();
+async fn get_ip(headers: HeaderMap) -> Json<String> {
+    let ip: String = headers.get("CF-Connecting-IP").unwrap().to_str().unwrap().parse().unwrap();
     return Json(ip);
 }
 
